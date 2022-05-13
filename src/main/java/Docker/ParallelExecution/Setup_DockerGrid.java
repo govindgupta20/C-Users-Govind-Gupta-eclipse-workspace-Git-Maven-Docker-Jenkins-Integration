516f77e79cc74714829fc2b935123f8e0e8d874f
Govind Gupta <govindgupta020990@gmail.com>
2022-05-13 16:44:23 +0530

    configured docker grid in testng.xml

--- src/main/java/Docker/ParallelExecution/Setup_DockerGrid.java
+++ src/main/java/Docker/ParallelExecution/Setup_DockerGrid.java
@@ -2,14 +2,15 @@ package Docker.ParallelExecution;
 
 import org.testng.annotations.AfterSuite;
 import org.testng.annotations.BeforeSuite;
-import org.testng.annotations.Test;
 
 public class Setup_DockerGrid {
 
 @BeforeSuite
 public void Start_DockerGrid() throws Exception {
+	System.out.println("Starting Docker");
 	Runtime.getRuntime().exec("cmd /c start Start_DockerGrid.bat");
 	Thread.sleep(30000);
+	System.out.println("Docker Started...procedding after 30 sec wait");
 	
 }
 
@@ -17,8 +18,10 @@ public void Start_DockerGrid() throws Exception {
 @AfterSuite
 public void Stop_DockerGrid() throws Exception {
 	Runtime.getRuntime().exec("cmd /c start Stop_DockerGrid.bat");
+	System.out.println("Stoppind Docker");
 	Thread.sleep(10000);
 	Runtime.getRuntime().exec("taskkill /f /im cmd.exe");
+	System.out.println("Docker container stopped");
 	
 }
 	
